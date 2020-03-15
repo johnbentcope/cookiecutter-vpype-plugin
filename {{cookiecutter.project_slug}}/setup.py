1f4d53286fc2cb81d3f29008c343b89506f17ffa
Antoine Beyeler <abeyeler@ab-ware.com>
2020-03-15 16:30:25 +0100

    Fixed setup.py to avoid using find_package (causes install problem for some reason)

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 
 with open("README.md") as f:
@@ -16,7 +16,7 @@ setup(
     author="{{cookiecutter.author_name}}",
     url="",
     license=license,
-    packages=find_packages(exclude=("examples", "tests")),
+    packages=["{{project_identifier}}"],
     install_requires=[
         'click',
         'vpype @ git+https://github.com/abey79/vpype.git',
